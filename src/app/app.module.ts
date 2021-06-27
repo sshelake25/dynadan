fcba41c9c7e1cb773f79644bdbc4fe0a32b43c21
Renuka S <renuka_somagutta@mohawkind.com>
2021-06-27 23:57:09 +0530

    adnt lib added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppComponent } from './app.component';
 import { FabricjsEditorModule } from 'projects/angular-editor-fabric-js/src/public-api';
 import { FormsModule } from '@angular/forms';
 import { ColorPickerModule } from 'ngx-color-picker';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -14,9 +22,11 @@ import { ColorPickerModule } from 'ngx-color-picker';
     BrowserModule,
     FabricjsEditorModule,
     FormsModule,
-    ColorPickerModule
+    ColorPickerModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
